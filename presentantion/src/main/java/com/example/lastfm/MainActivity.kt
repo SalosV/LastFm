8ec8b45ab48a82850cd9b59dd2d64055067bea36
Santiago Lozada <santiagolozada95@gmail.com>
2020-05-04 18:46:45 -0500

    Creation of fragment tracks and artists.
    Added base url.
    
    Add ApiKey in file api.properties

--- presentantion/src/main/java/com/example/lastfm/MainActivity.kt
+++ presentantion/src/main/java/com/example/lastfm/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.lastfm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.lastfm.Artists.ArtistsFragment
+import com.example.lastfm.tracks.TracksFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigationBottomItemSelect()
+    }
+
+    private fun navigationBottomItemSelect() {
+        navView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_artists -> {
+                    replace(ArtistsFragment())
+                }
+                R.id.navigation_tracks -> {
+                    replace(TracksFragment())
+                }
+            }
+            true
+        }
+    }
+
+    private fun replace(fragmentClass: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .setCustomAnimations(
+                R.anim.slide_in_right,
+                android.R.anim.fade_out
+            )
+            .replace(R.id.frame_layout, fragmentClass).commit()
     }
 }
